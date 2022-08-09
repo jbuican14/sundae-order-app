d2793a40fa79d217ca573dffad3091f744007f15
Juti Buican <jbuican19@gmail.com>
2022-08-09 19:24:39 +0100

    Add test for scoop option images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
+import ScoopOption from "./pages/entry/ScoopOption";
 import SummaryForm from "./pages/summary/SummaryForm";
 
 function App() {
-  return <SummaryForm />;
+  return (
+    <>
+      <ScoopOption />
+      <SummaryForm />
+    </>
+  );
 }
 
 export default App;
